ebe64b67e8fb1d3658781cc790526675dc62839e
DECIO SANTANA DE AGUIAR <88034016+Decioagu@users.noreply.github.com>
2023-08-04 11:04:40 -0300

    correção ortográfica

--- 008.py
+++ 008.py
@@ -2,8 +2,8 @@
 Faça uma lista de comprar com listas
 O usuário deve ter a possibilidade de
 inserir, apagar e listar valores de sua lista
-Não permita que o programa quebe com
-erros de ídices inexistentes na lsiata.
+Não permita que o programa quebre com
+erros de índices inexistentes na lista.
 '''
 import os
 
